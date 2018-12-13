a9e960b3a816cf9bf6050f9a6c38ef72b4922ab2
myih <myih@ucdavis.edu>
2018-12-13 11:17:06 -0800

    Run train & eval (legacy)
    Need to change address

---
+++ research/scripts/ssd_mobilenet_eval.sh
@@ -0,0 +1,5 @@
+python /home/matthew/models/research/object_detection/legacy/eval.py \
+--logtostderr --checkpoint_dir=/home/matthew/ssd_mobilenet_tensorflow/train \
+--pipeline_config_path=/home/matthew/ssd_mobilenet_tensorflow/models/ssd_mobilenet_gtsdb.config  \
+--eval_dir=/home/matthew/ssd_mobilenet_tensorflow/eval
+
---
+++ research/scripts/ssd_mobilenet_train.sh
@@ -0,0 +1,5 @@
+ python /home/matthew/models/research/object_detection/legacy/train.py \
+ --train_dir=/home/matthew/ssd_mobilenet_tensorflow/train \
+ --pipeline_config_path=/home/matthew/ssd_mobilenet_tensorflow/models/ssd_mobilenet_gtsdb.config \
+ -alsologtostderr
+
