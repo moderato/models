1576ec6a7c25b38e0336936d168163884870aab4
moderato <moderato926@gmail.com>
2018-12-15 00:21:29 -0800

    Add config file for SSD-MobileNetV1 on GTSDB

--- research/scripts/export_tf.sh
+++ research/scripts/export_tf.sh
@@ -1,7 +1,7 @@
 cd ..
 INPUT_TYPE=image_tensor
 PIPELINE_CONFIG_PATH=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_mobilenet_v1/train_logs/pipeline.config
-TRAINED_CKPT_PREFIX=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_mobilenet_v1/train_logs/model.ckpt-200000
+TRAINED_CKPT_PREFIX=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_mobilenet_v1/train_logs/model.ckpt-120000
 EXPORT_DIR=$OPENVINO_ROOTDIR/deployment_tools/model_optimizer_R3/gtsdb/tf/MobileNetV1
 python object_detection/export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
