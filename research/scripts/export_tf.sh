b24a3016bcd53eb3dbae577655a9240b9f958f4c
moderato <moderato926@gmail.com>
2019-03-17 10:42:41 -0700

    Add SqueezeNet v1.1 and VGG16 support

--- research/scripts/export_tf.sh
+++ research/scripts/export_tf.sh
@@ -1,9 +1,8 @@
 cd ..
 INPUT_TYPE=image_tensor
-PIPELINE_CONFIG_PATH=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_mobilenet_v1/train_logs/pipeline.config
-TRAINED_CKPT_PREFIX=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_mobilenet_v1/train_logs/model.ckpt-120004
-# EXPORT_DIR=$OPENVINO_ROOTDIR/deployment_tools/model_optimizer_R3/gtsdb/tf/MobileNetV1
-EXPORT_DIR=.
+PIPELINE_CONFIG_PATH=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_vgg16/train_logs/pipeline.config
+TRAINED_CKPT_PREFIX=$HOME/Documents/TrafficSignBench/Detection/models/research/object_detection/trained_models/gtsdb_ssd_vgg16/train_logs/model.ckpt-60000
+EXPORT_DIR=$OPENVINO_ROOTDIR/deployment_tools/model_optimizer_R3/gtsdb/tf/VGG16
 python object_detection/export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
